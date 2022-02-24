1d1a9885035202259615e7791f381550c296ce9b
adhir <adhiruchil@gmail.com>
2022-02-24 08:56:01 +0530

    frontend init fix

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="text-red bg-white" height-hint="98">
+      <q-toolbar>
+        <router-link to="/">
+          <img src="~assets/recipeezy.png" class="cursor-pointer" />
+        </router-link>
+
+        <q-tabs align="right">
+          <q-route-tab to="/search" label="Search" />
+          <q-route-tab to="/create" label="Create" />
+        </q-tabs>
+        <q-space />
+        <q-btn stretch flat to="/login" label="Login" />
+        <q-btn stretch flat to="/register" label="Create Account" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ frontend/src/pages/Search.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <p>Search Page</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageSearch'
+})
+</script>
