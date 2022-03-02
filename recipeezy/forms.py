3f5967fdbeb89b5413847ec8a22d857e6f68f22f
dash2927 <dash2927@uw.edu>
2022-03-01 20:57:39 -0700

    Added form data

---
+++ recipeezy/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Optional
+
+
+class LoginForm(FlaskForm):
+    '''
+    Form to log in to account
+    '''
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class MakeAcctForm(FlaskForm):
+    '''
+    Form to make an account
+    '''
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[Optional()])
+    submit = SubmitField('Create Account')
+
+
+class SubmitRecForm(FlaskForm):
+    '''
+    Form to Submit a Recipe
+    '''
+    title = StringField('Recipe Title', validators=[DataRequired()])
+    recipe = TextAreaField('Recipe', validators=[DataRequired()])
+    submit = SubmitField('Submit')
