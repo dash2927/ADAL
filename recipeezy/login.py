48ed59ebdff3d7263b28f155b934eb1887f5b2f6
dash2927 <dash2927@uw.edu>
2022-03-03 05:06:10 -0700

    Working login validation and redirect

--- recipeezy/login.py
+++ recipeezy/login.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, abort, flash, redirect, session, render_template, request, url_for
 from flask import current_app as ca
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, is_safe_url
+from flask_login import login_user, current_user
+from urllib.parse import urlparse, urljoin
 from .database import User
 from .forms import LoginForm, MakeAcctForm
 import os
@@ -16,6 +17,13 @@ loginbp = Blueprint('login_bp',
                     static_folder='static')
 
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+        ref_url.netloc == test_url.netloc
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.objects.get(username=user_id)
@@ -23,18 +31,20 @@ def load_user(user_id):
 
 @loginbp.route('/login', methods=('GET', 'POST'))
 def login():
-    user = request.current_user
+    user = current_user
     form = LoginForm()
     if form.validate_on_submit():
+        print(user, flush=True)
         login_user(user)
         flash('Logged in succesfully')
-
-        next = request.args.get('next')
+        # next gets the page you were previously on for redirect
+        nxt = request.args.get('next')
         # is_safe_url should check if the url is safe for redirects.
-        if not is_safe_url(next):
+        if not is_safe_url(nxt):
             return abort(400)
-
-        return redirect(next or url_for('home_bp.home'))
+        return redirect(nxt or url_for('home_bp.home'))
+    print(form.errors, flush=True)
+    print(current_user, flush=True)
     return render_template('login.html', form=form)
 
 
