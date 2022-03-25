a23e30083ae3b51af66cc7ab31eb6e07baced1fa
adhir <adhiruchil@gmail.com>
2022-03-25 19:50:43 +0530

    frontend logic for create recipe page

--- recipeezy/create.py
+++ recipeezy/create.py
@@ -6,51 +6,71 @@ from urllib.parse import urlparse, urljoin
 from .database import User, Post
 from .forms import LoginForm, SubmitRecForm
 import os
+import json
+import time
 from . import login_manager, db
+from werkzeug.utils import secure_filename
+
 # from database import db
 
 createbp = Blueprint('create_bp',
                      __name__,
-                     # url_prefix='/',
+                    #  url_prefix='/',
                      template_folder='templates',
                      static_folder='static')
 
-
-def menu_selection(req, form):
-    # if 'username' in session:
-    #     username = escape(session['username'])
-    # else:
-    #     username = None
-    if req.form.get('menu', None):
-        return redirect(url_for('home_bp.home'))
-    elif req.form.get('login', None):
-        return redirect(url_for('login_bp.login'))
-    elif req.form.get('logout', None):
-        return redirect(url_for('login_bp.logout'))
-    elif req.form.get('create', None):
-        return redirect(url_for('create_bp.create'))
-    # elif req.form.get('search', None):
-    #     return redirect(url_for('search', username=username))
-    return render_template('create.html', form=form)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ca.config['ALLOWED_EXTENSIONS']
 
 
 @createbp.route('/create', methods=('GET', 'POST'))
-@login_required
+# @login_required
 def create():
-    print("Switched to create", flush=True)
-    form = SubmitRecForm()
-    if form.validate_on_submit():
-        print("---Submit button pressed", flush=True)
-        title = request.form['title']
-        recipe = request.form['recipe']
-        if Post.query.filter_by(title=title).first() is not None:
-            flash("Recipe already in db")
-        else:
-            post = Post(current_user.id, title)  # add post to db
-            current_user.posts += 1
-            post.body = recipe
-            db.session.add(post)
-            db.session.commit()
-    elif request.method == 'POST':
-        return menu_selection(request, form=form)
-    return render_template('create.html', form=form)
+    # form = SubmitRecForm()
+    # if form.validate_on_submit():
+    #     print("---Submit button pressed", flush=True)
+    #     title = request.form['title']
+    #     recipe = request.form['recipe']
+    #     if Post.query.filter_by(title=title).first() is not None:
+    #         flash("Recipe already in db")
+    #     else:
+    #         post = Post(current_user.id, title)  # add post to db
+    #         current_user.posts += 1
+    #         post.body = recipe
+    #         db.session.add(post)
+    #         db.session.commit()
+    if request.method == 'POST':
+        data = request.form['submitData']
+        data = json.loads(data)
+        file = request.files.get('file')
+
+        name = data.get('name')
+        category = data.get('category')
+        description = data.get('description')
+        tags = data.get('tags')
+        steps = data.get('steps')
+        ingredients = data.get('ingredients')
+
+        print(steps)
+        print(type(steps))
+
+        print(ingredients[1])
+        
+        if not (name and category and description):
+            return {'status': -1, 'message': 'Please fill every field'}
+
+        if file:
+            if not allowed_file(file.filename):
+                return {'status': -1, 'message': 'Invalid image file format'}
+
+            timestamp = int(time.time())
+            filename = file.filename.rsplit(".", 1)
+            filename = filename[0] + str(timestamp) + "." + filename[1]
+            file.save(os.path.join(ca.config['UPLOAD_FOLDER'], filename))
+
+        # Add insert to database code here
+
+        return {'status': 0, 'message': 'success'}
+        
+    return render_template('create.html')
