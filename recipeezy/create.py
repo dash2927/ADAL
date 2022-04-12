de9bda559ae25c0663eb2e81eb3f3770f7beecda
dash2927 <david.c.shackelford@gmail.com>
2022-04-12 04:28:10 -0600

    aws: app configuration error

--- recipeezy/create.py
+++ recipeezy/create.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse, urljoin
 from werkzeug.utils import secure_filename
 from .database import User, Post
 from .forms import LoginForm, SubmitRecForm
-from . import login_manager, db
+from . import login_manager, db, app
 
 # from database import db
 
