01aa04bd42b5a9a8c740e22355a056b5cac59fab
alexplazas <alexplazas@gmail.com>
2022-04-24 16:52:30 -0600

    more docs

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,84 +1,115 @@
+# Import Flask components and app db definitions
+# Import unit test framework
 from flask_testing import TestCase
 from flask import Flask
 from recipeezy import db
 from recipeezy.database import User, Post
-
 import unittest
 import os
 
-
+# Define new SQLTest class extending test case
 class SQLTestCase(TestCase):
 
+    # Method to set up the test class
     @classmethod
     def setUpClass(cls):
         pass
-
+    
+    # Method to tear down the test class on completion
     @classmethod
     def tearDownClass(cls):
         pass
-
+    
+    # Method to set up the test case on each run, taking the current test
     def setUp(self):
+        # Create app using current test fixture
         self.create_app()
+        # Initialize the app and create all necessary dbs
         db.init_app(self.app)
         with self.app.app_context():
             db.create_all()
 
+    # Method to tear down the test case after each run
     def tearDown(self):
+        # Remove all db connections and drop all dbs
         db.session.remove()
         db.drop_all()
+        # Remove the testing db
         os.remove('tests/sqltesting.sqlite3')
 
+    # Helper method for creating the app and passing params
     def create_app(self):
+        # Create a new app instance and set necessary config options for testing
         app = Flask(__name__)
         app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sqltesting.sqlite3"
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['TESTING'] = True
+        # Return new app instance
         return app
 
+    # Test for adding a user to the User db
     def test_add_user(self):
         print()
         print("Testing adding a user")
+        # Create two new user entities
         first_user = User("First_User", "password1")
         second_user = User("Second_User", "password2")
+        # Add user entities to the db; commit changes
         db.session.add(first_user)
         db.session.add(second_user)
         db.session.commit()
+        # Assert that first user is in the db
         assert first_user in db.session
         user = User.query.filter_by(uname="First_User").first()
+        # Assert that the username of the user in the db is matching
         self.assertEqual(user.username, "First_User",
                          msg="First_User not present")
 
+    # Test for adding emails to User db and any modifications
     def test_email(self):
         print()
         print("Testing adding and changing email")
+        # Create two new user entities
         first_user = User("First_User", "password1")
         second_user = User("Second_User", "password2")
+        # Add user entities to the db; commit changes
         db.session.add(first_user)
         db.session.add(second_user)
         db.session.commit()
+        # Create array of invalid email formats
         bademails = ['bademail', 'bademail@', 'BADEMAIL@C.C', '']
+        # Get user from the User db
         user = User.query.filter_by(uname="First_User").first()
+        # Assert that the user is present in the db
         self.assertEqual(user.username, "First_User",
                          msg="First_User not present")
+        # Assert that an error is raised if invalid email is set on a user
         with self.assertRaises(ValueError):
             for i in bademails:
                 user.email = i  # Testing for bad emails
+        # Create array of valid email formats
         goodemails = ["good@email.com", "GOOD@EMAIL.COM", "A@B.edu"]
         user = User.query.filter_by(id=2).first()  # Get second user
+        # Set the email of the test user with valid emails
         for i in goodemails:
             user.email = i
             db.session.commit()
+        # Assert that user is in the db and that the email is updated
         self.assertEqual(user.username, "Second_User")
         self.assertEqual(user._email, "A@B.edu")  # testing email change
 
+    # Test for setting password on a User entity and adding to db
     def test_password(self):
         print()
         print("Testing adding and changing password")
+        # Create two new user entities
         first_user = User("First_User", "password1")
         second_user = User("Second_User", "password2")
+        # Add user entities to the db; commit changes
         db.session.add(first_user)
         db.session.add(second_user)
         db.session.commit()
+        # Get user from the User db, assert that username and password fields are valid
         user = User.query.filter_by(uname="First_User").first()
         self.assertEqual(user.username, "First_User",
                          msg="First_User not present")
@@ -89,6 +120,7 @@ class SQLTestCase(TestCase):
             print(user.password)  # Cannot get a psswd
             user.password = 1
             user.password = []
+        # Set new password on the second user, assert that username and password match
         user.password = "new_password"
         self.assertEqual(user.username, "Second_User")
         self.assertTrue(user.verify_pwd("new_password"),
--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,5 +1,5 @@
-import pytest
 
+import pytest
 from recipeezy.database import db
 
 # Test for getting default page from base directory
